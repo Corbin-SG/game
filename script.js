888fc6a4a4fcb97580ee236a4a5b4a32099a89b9
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2024-04-29 11:29:07 -0400

    Added text to about page.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var infoButton = document.getElementById("info");
 var startButton = document.getElementById("start");
 var background = document.getElementById("background");
+var infoText = document.getElementById("information");
 
 infoButton.addEventListener("click", function() {
     background.style.backgroundImage="url(infoScreen.png)";
     infoButton.hidden = true
     startButton.hidden = true
+    infoText.hidden = false
 });
