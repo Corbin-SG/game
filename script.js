935e95faf7529c29968d35010dab2e6cef79720e
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2024-04-29 11:51:45 -0400

    Tweaked info label visibility

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var infoButton = document.getElementById("info");
 var startButton = document.getElementById("start");
 var background = document.getElementById("background");
 var infoText = document.getElementById("information");
+var infoLabel = document.getElementById("label");
 
 infoButton.addEventListener("click", function() {
     background.style.backgroundImage="url(infoScreen.png)";
     infoButton.hidden = true
     startButton.hidden = true
     infoText.hidden = false
+    infoLabel.hidden = false
 });
