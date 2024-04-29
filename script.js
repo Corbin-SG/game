5dd6756451bbfc817801063bedabb5bf3e2456c7
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2024-04-29 15:02:05 -0400

    Tweaked background scaling

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var infoText = document.getElementById("information");
 var infoLabel = document.getElementById("label");
 
 infoButton.addEventListener("click", function() {
-    background.style.backgroundImage="url(infoScreen.png)";
+    document.body.style.backgroundImage ="url(infoScreen.png)";
     infoButton.hidden = true
     startButton.hidden = true
     infoText.hidden = false
