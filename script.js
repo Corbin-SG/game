93a66f86e4511600f3a3c45ba81c252a98d35f9e
Oliver Gingerich <Ogingerich5354@conestogac.on.ca>
2024-04-30 11:36:18 -0400

    Added functionality to start button.

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ var infoButton = document.getElementById("info");
 var startButton = document.getElementById("start");
 var backToMainButton = document.getElementById("backToMain");
 var howToButton = document.getElementById("howTo");
+var easyButton = document.getElementById("easy");
+var regularButton = document.getElementById("regular");
+var hardButton = document.getElementById("hard");
+var insaneButton = document.getElementById("insane");
 var infoText = document.getElementById("information");
 var infoLabel = document.getElementById("label");
 
 infoButton.addEventListener("click", function() {
-    document.body.style.backgroundImage ="url(infoScreen.png)";
+    document.body.style.backgroundImage ="url(grayScreen.png)";
     document.body.style.border = "2vw solid #26153A"
     infoButton.hidden = true
     startButton.hidden = true
@@ -25,4 +29,19 @@ backToMainButton.addEventListener("click", function(){
     infoLabel.hidden = true
     backToMainButton.hidden = true
     howToButton.hidden = true
+    easyButton.hidden = true
+    regularButton.hidden = true
+    hardButton.hidden = true
+    insaneButton.hidden = true
+})
+
+startButton.addEventListener("click", function(){
+    document.body.style.backgroundImage = "url(startScreen.png)";
+    infoButton.hidden = true
+    startButton.hidden = true
+    backToMainButton.hidden = false
+    easyButton.hidden = false
+    regularButton.hidden = false
+    hardButton.hidden = false
+    insaneButton.hidden = false
 })
